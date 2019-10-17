b9b6cd20517998652b9a712296ffaacb299ca894
geleazar1000111 <geraldine@osaro.com>
2019-10-17 11:53:31 -0700

    put code to access user calendar in functions and got event color

--- gcal_tool.py
+++ gcal_tool.py
@@ -11,10 +11,7 @@ from google.auth.transport.requests import Request
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
+def get_creds():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -33,21 +30,33 @@ def main():
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
+    return creds
+
+def get_events():
+    creds = get_creds()
 
     service = build('calendar', 'v3', credentials=creds)
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
+    colors = service.colors().get(fields = 'event').execute()
+    events_result = service.events().list(calendarId='primary', timeMin=now, maxResults =10,
                                         singleEvents=True, orderBy='startTime').execute()
-    events = events_result.get('items', [])
+    events = events_result.get('items', []) 
+    return events, colors
 
+def main():
+    events = get_events()[0]
+    colors = get_events()[1]
     if not events:
         print('No upcoming events found.')
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'], event['colorId'])
+        if 'colorId' in event:
+            clr = colors['event'][event['colorId']]['background']
+        else:
+            clr = "None"
+        print(start, event['summary'], clr)
 
 if __name__ == '__main__':
     main()
