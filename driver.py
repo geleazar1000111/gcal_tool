2b88cf60e7124e86999f8c61606b2be906a5178e
geleazar1000111 <56564400+geleazar1000111@users.noreply.github.com>
2019-10-24 21:11:31 -0700

    Update driver.py
    added ValueError for input checking

--- driver.py
+++ driver.py
@@ -1,22 +1,21 @@
-import csv
+mport csv
 import gcal_tool
 
 
 def main():
     try:
-        max_results = input("Please enter the number of events you would like to get: ")
-        assert type(max_results) == int
-        event_map = gcal_tool.create_event_color_map(max_results)
-        with open('gcal_tool_results', 'w', newline='') as csvfile:
-            gcal_writer = csv.writer(csvfile)
-            gcal_writer.writerow(
-                ["Event name"] + ["Color"] + ["Start"] + ["End"] + ["Time spent (hours)"] + ["Time free (hours)"])
-            for key in event_map:
-                gcal_writer.writerow([key] + event_map[key])
-        csvfile.close()
-    except AssertionError:
+        max_results = int(input("Please enter the number of events you would like to get: "))
+        assert isinstance(max_results, int)
+    except ValueError:
         print("Incorrect form of input (must be int)")
-
+    event_map = gcal_tool.create_event_color_map(max_results)
+    with open('gcal_tool_results', 'w', newline='') as csvfile:
+        gcal_writer = csv.writer(csvfile)
+        gcal_writer.writerow(
+            ["Event name"] + ["Color"] + ["Start"] + ["End"] + ["Time spent (hours)"] + ["Time free (hours)"])
+        for key in event_map:
+            gcal_writer.writerow([key] + event_map[key])
+    csvfile.close()
 
 
 if __name__ == '__main__':
