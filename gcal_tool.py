6c1f5169e91e87dd27a12044a6616bb387aef2ed
geraldineeleazar <geraldine@osaro.com>
2019-12-18 12:42:59 -0800

    set boundaries

--- gcal_tool.py
+++ gcal_tool.py
@@ -1,18 +1,22 @@
-#add change comments here
+# add change comments here
 from __future__ import print_function
 import re
 import datetime
 import pickle
 import os.path
+import math
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # Modified from Google Calendar API Quickstart
 VALID_DATETIME_FORMATS = ('%Y-%m-%dT%H:%M:%S%f', '%Y-%m-%d')
+WORK_START = datetime.datetime.strptime("08:00:00", "%H:%M:%S").time()
+WORK_END = datetime.datetime.strptime("17:00:00", "%H:%M:%S").time()
 WORKING_HOURS = 9
+CENTURY = 21
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
 def get_creds():
@@ -49,6 +53,27 @@ def get_events(max_results):
     return events, colors
 
 
+def get_day_of_week(event_date):
+    '''Uses this formula from https://cs.uwaterloo.ca/~alopez-o/math-faq/node73.html to determine day of week from date'''
+    '''Monday = 1, Tuesday = 2, etc.'''
+    day = event_date.day
+    month = event_date.month
+    year = event_date.year
+    t = [0, 3, 2, 5, 0, 3,
+         5, 1, 4, 6, 2, 4]
+    year -= month < 3
+    return ((year + int(year / 4) - int(year / 100)
+             + int(year / 400) + t[month - 1] + day) % 7)
+
+
+def is_between_working_hrs(time):
+    return WORK_START <= time <= WORK_END
+
+
+def filter_events(events):
+    pass
+
+
 def is_single_day_event(datetime_str):
     regex_obj = re.compile('.*-.*-.*T.*:.*:.*')
     if regex_obj.match(datetime_str):
@@ -64,6 +89,7 @@ def convert_google_datetime(datetime_str):
 
 
 def calculate_hours_free(hours_spent):
+    '''TODO: determine when robot calendar is booked'''
     return WORKING_HOURS - hours_spent
 
 
@@ -100,3 +126,12 @@ def create_event_color_list(max_results):
             clr = "None"
         event_color_list.append([name, clr, start, end, hours_spent])
     return event_color_list
+
+def main():
+    events_list = get_events(1)[0]
+    start = convert_google_datetime(get_start(events_list[0]))
+    weekday = get_day_of_week(start)
+    print(start.time(), type(start.time()))
+    print(is_between_working_hrs(start.time()))
+
+main()
